d16bc4d7d45be9f6e4cc422052cff40cdf732404
jstenger <jake.stenger.19@bishops.com>
2019-10-30 16:04:20 -0700

    added generate-scales python script and scales.json output file

---
+++ generate-scales.py
@@ -0,0 +1,22 @@
+# generates a json file containing musical notes and their frequencies in equal temperament tuning
+
+import json
+
+
+basefrequency = 27.5
+scaleDict = {}
+notes = [ 'A', 'A#', 'B', 'C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#']
+
+# for i in range(12):
+
+for octave in range(8):
+    for halfstep in range(12):
+
+        if(halfstep > 2):
+            scaleDict[round(basefrequency * (2**(octave + halfstep / 12)), 2)] = notes[halfstep] + str(octave + 1)
+        else:
+            scaleDict[round(basefrequency * (2**(octave + halfstep / 12)), 2)] = notes[halfstep] + str(octave)
+
+with open("scales.json", 'w') as scales:
+    json.dump(scaleDict, scales)
+
