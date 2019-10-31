55b2fb69001348c12e909f65ab133630d8a9787c
jstenger <jake.stenger.19@bishops.com>
2019-10-31 10:05:18 -0700

    cleaned up styling of data display

--- docs/generate-scales.py
+++ docs/generate-scales.py
@@ -2,7 +2,7 @@
 import json
 
 basefrequency = 27.5
-scaleDict = {}
+scaleDict = {"frequencies": [], "notes": []}
 notes = [ 'A', 'A#', 'B', 'C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#']
 
 # for i in range(12):
@@ -11,10 +11,12 @@ for octave in range(8):
     for halfstep in range(12):
 
         if(halfstep > 2):
-            scaleDict[round(basefrequency * (2**(octave + halfstep / 12)), 2)] = notes[halfstep] + str(octave + 1)
+            scaleDict["frequencies"].append(round(basefrequency * (2**(octave + halfstep / 12)), 2))
+            scaleDict["notes"].append(notes[halfstep] + str(octave + 1))
         else:
-            scaleDict[round(basefrequency * (2**(octave + halfstep / 12)), 2)] = notes[halfstep] + str(octave)
+            scaleDict["frequencies"].append(round(basefrequency * (2 ** (octave + halfstep / 12)), 2))
+            scaleDict["notes"].append(notes[halfstep] + str(octave))
 
-with open("scales.js", 'w') as scales:
+with open("const scales.js", 'w') as scales:
     scales.write("scales = " + json.dumps(scaleDict))
 
