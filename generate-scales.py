a6ce01eb1f49d088731ca53bb0c90c2e21af8f9b
jstenger <jake.stenger.19@bishops.com>
2019-10-31 10:46:47 -0700

    moved generate-scales

--- generate-scales.py
+++ generate-scales.py
@@ -1,10 +1,8 @@
 # generates a json file containing musical notes and their frequencies in equal temperament tuning
-
 import json
 
-
 basefrequency = 27.5
-scaleDict = {}
+scaleDict = {"frequencies": [], "notes": []}
 notes = [ 'A', 'A#', 'B', 'C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#']
 
 # for i in range(12):
@@ -13,10 +11,12 @@ for octave in range(8):
     for halfstep in range(12):
 
         if(halfstep > 2):
-            scaleDict[round(basefrequency * (2**(octave + halfstep / 12)), 2)] = notes[halfstep] + str(octave + 1)
+            scaleDict["frequencies"].append(round(basefrequency * (2**(octave + halfstep / 12)), 2))
+            scaleDict["notes"].append(notes[halfstep] + str(octave + 1))
         else:
-            scaleDict[round(basefrequency * (2**(octave + halfstep / 12)), 2)] = notes[halfstep] + str(octave)
+            scaleDict["frequencies"].append(round(basefrequency * (2 ** (octave + halfstep / 12)), 2))
+            scaleDict["notes"].append(notes[halfstep] + str(octave))
 
-with open("scales.json", 'w') as scales:
-    json.dump(scaleDict, scales)
+with open("const scales.js", 'w') as scales:
+    scales.write("scales = " + json.dumps(scaleDict))
 
